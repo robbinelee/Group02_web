65e9679460fb31fc41c7fae2c705fc8150e9026c
mumou1314 <463260310@qq.com>
2018-08-14 20:51:07 +0800

    peizhikuangjia

--- src/main/java/www/gezhi/domain/User.java
+++
@@ -1,7 +0,0 @@
-package www.gezhi.domain;
-
-public class User {
-    private String username;
-    private  String id;
-    private  String password1;
-}
--- src/main/java/www/gezhi/test/hello.java
+++
@@ -1,7 +0,0 @@
-package www.gezhi.test;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("aaaaassssssssssaaaa");
-    }
-}
