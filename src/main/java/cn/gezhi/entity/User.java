09051e2ba43a9a786590da800d8b8103a4e8fe5c
Administrator <2014bijiehun>
2018-08-14 17:55:14 +0800

    新增User

--- src/main/java/cn/gezhi/entity/User.java
+++ src/main/java/cn/gezhi/entity/User.java
@@ -1,4 +1,49 @@
 package gezhi.entity;
 
 public class User {
+    private Integer id;
+    private String username;
+    private String password;
+
+    public java.lang.Integer getId() {
+        return id;
+    }
+
+    public void setId(java.lang.Integer id) {
+        this.id = id;
+    }
+
+    public java.lang.String getUsername() {
+        return username;
+    }
+
+    public void setUsername(java.lang.String username) {
+        this.username = username;
+    }
+
+    public java.lang.String getPassword() {
+        return password;
+    }
+
+    public void setPassword(java.lang.String password) {
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public User(java.lang.Integer id, java.lang.String username, java.lang.String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username=" + username +
+                ", password=" + password +
+                '}';
+    }
 }
