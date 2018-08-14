440b09ee518c03b2bbe85e9fd9f2bbb24c0e2331
snakess1314 <986529221@qq.com>
2018-08-14 17:52:41 +0800

    test

--- src/main/java/www/gezhi/test/hello.java
+++ src/main/java/www/gezhi/test/hello.java
@@ -2,6 +2,6 @@ package www.gezhi.test;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("aaaaaa");
+        System.out.println("aaaaaaaaa");
     }
 }
